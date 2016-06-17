a08559397b43b4c50688399faf2f5bb35905dd25
Aparna Reddy Challa <aparnareddychalla@aparnas-macbook-pro.local>
2016-06-17 11:12:35 +0530

    Initial Commit

---
+++ iMediHubLIMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iMediHubLIMS
+//
+//  Created by Aparna Reddy Challa on 17/06/16.
+//  Copyright © 2016 Aparna Reddy Challa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iMediHubLIMS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iMediHubLIMS
+//
+//  Created by Aparna Reddy Challa on 17/06/16.
+//  Copyright © 2016 Aparna Reddy Challa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
